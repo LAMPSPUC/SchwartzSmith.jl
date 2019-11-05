c4e9775fc4c3adf75e29bd8db7d529b04c658bbe
Marina Dietze Monteiro <mdietze@ele.puc-rio.br>
2019-11-05 18:50:25 -0300

    Matrices definition and check positive semi-definite characteristic

---
+++ src/auxiliar.jl
@@ -0,0 +1,28 @@
+"""
+Definition of ensure_pos_sym functions to guarantee that some of the matrices
+are positive semi-definite.
+"""
+
+function ensure_pos_sym!(M::AbstractArray{T}, t::Int; ϵ::T = T(1e-8)) where T
+    @inbounds for j in axes(M, 2), i in 1:j
+        if i == j
+            M[i, i, t] = (M[i, i, t] + M[i, i, t])/2 + ϵ
+        else
+            M[i, j, t] = (M[i, j, t] + M[j, i, t])/2
+            M[j, i, t] = M[i, j, t]
+        end
+    end
+    return
+end
+
+function ensure_pos_sym!(M::AbstractArray{T}; ϵ::T = T(1e-8)) where T
+    @inbounds for j in axes(M, 2), i in 1:j
+        if i == j
+            M[i, i] = (M[i, i] + M[i, i])/2 + ϵ
+        else
+            M[i, j] = (M[i, j] + M[j, i])/2
+            M[j, i] = M[i, j]
+        end
+    end
+    return
+end
--- test/runtest.jl
+++ test/runtest.jl
@@ -1,3 +1,20 @@
 include("C:\\Users\\mdietze\\Dropbox\\PUC\\Eneva\\Schwartz-Smith\\SchwartzSmith.jl\\src\\SchwartzSmith.jl")
 
-SchwartzSmith.greet()
+k = 1.49
+sigma_chi = 0.286
+lambda_chi = 0.157
+mi_xi = -0.0125
+sigma_xi = 0.145
+mi_xi_star = 0.0115
+rho_xi_chi = 0.3
+
+T = [10;20]
+
+p = SchwartzSmith.SSParams(k,sigma_chi,lambda_chi,mi_xi,sigma_xi,mi_xi_star,rho_xi_chi)
+
+A_test = SchwartzSmith.A(20,p)
+W_test = SchwartzSmith.W(p)
+G_test = SchwartzSmith.G(p)
+c_test = SchwartzSmith.c(p)
+d_test = SchwartzSmith.d(T,p)
+F_test = SchwartzSmith.F(T,p)
