4879510782e3e241ddad2f0ebba7b69c3c292342
Marina Dietze Monteiro <mdietze@ele.puc-rio.br>
2019-11-05 18:52:09 -0300

    Schwartz Smith main code update

--- src/SchwartzSmith.jl
+++ src/SchwartzSmith.jl
@@ -1,7 +1,18 @@
 module SchwartzSmith
 
+using Optim
 
-greet() = print("Hello World!")
+include("create_matrices.jl")
+include("auxiliar.jl")
+
+"""
+The Schwartz-Smith model equations are:
+Observation equation: y_t = d_t + F'_t*x_t + v_t
+State equation: x_t = c + G*x_t-1 + ω_t
+
+For each trading period t, we use a set of forward contracts with
+differents times to maturity T_1,...T_n. The default value for Δt is 1.
+"""
 
 
 end # module
