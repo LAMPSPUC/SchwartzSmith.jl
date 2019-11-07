8c7eb391f5a63a414a71ab62d4a4139270525543
Marina Dietze Monteiro <mdietze@ele.puc-rio.br>
2019-11-07 19:17:17 -0300

    schwartzsmith function update

--- src/SchwartzSmith.jl
+++ src/SchwartzSmith.jl
@@ -17,24 +17,12 @@ include("simulation.jl")
 
 Estimation of the Schwartz Smith model. Returns the estimated parameters.
 """
-function schwartzsmith(ln_F::Matrix{Float64}, T::Matrix{Float64}; delta_t::Int = 1)
+function schwartzsmith(ln_F::Matrix{Typ}, T::Matrix{Typ}; delta_t::Int = 1, seed::Vector{Typ} = -0.2*rand(Typ, 7 + size(ln_F, 2))) where Typ
 
     # Parameters estimation
     n, prods = size(ln_F)
 
-    #seed = -0.2*rand(7 + maximum(prods))
-    seed = [ -0.17225633886841765
- -0.09560442858060064
- -0.020985663535811397
- -0.0653045700223347
- -0.19660560937984536
- -0.12010844857257884
- -0.17418745092390253
- -0.14247585602034882
- -0.04342579875495778]
     optseed = optimize(psi -> compute_likelihood(ln_F, T, psi, delta_t), seed, LBFGS(), Optim.Options(f_tol = 1e-6, g_tol = 1e-6, show_trace = true))
-
-    println(optseed)
 
     opt_param = optseed.minimizer
 
@@ -49,8 +37,7 @@ function schwartzsmith(ln_F::Matrix{Float64}, T::Matrix{Float64}; delta_t::Int =
     s = exp.(opt_param[8:end])
     p = SSParams(k, sigma_chi, lambda_chi, mi_xi, sigma_xi, mi_xi_star, rho_xi_chi, s)
 
-    return p, seed
-
+    return p, seed, optseed
 end
 
 """
